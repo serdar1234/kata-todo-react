be7c77d34041afca1658c0eb917e4e04f9dff2e0
Serdar Geldyev <serdarabad@gmail.com>
2025-01-18 23:20:05 +0300

    format distance to now from date fns added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,25 @@
 import React, { Component, Fragment } from 'react';
+import { formatDistanceToNow } from 'date-fns';
 import '../../index.css';
 import Header from '../header';
 import TaskList from '../task-list/task-list';
 import Footer from '../footer';
 
 export default class App extends Component {
-
+  d = new Date(2025, 0, 18);
   state = { todoData: [
-    { liClass: "completed", description: "Completed task 1", createdAt: "created 17 seconds ago", id: 1},
-    { liClass: "editing", description: "Editing task", createdAt: "created 5 minutes ago", id: 2},
-    { liClass: null, description: "Active task", createdAt: "created 5 minutes ago", id: 3},
+    { liClass: "completed", description: "Completed task 101", 
+      createdAt: formatDistanceToNow(this.d, {includeSeconds: true}), id: 1},
+    { liClass: "editing", description: "Editing task", 
+      createdAt: this.printTime(this.d.setDate(this.d.getDate() - 5)), id: 2},
+    { liClass: null, description: "Active task",
+      createdAt: this.printTime(this.d), id: 3},
   ]}
+
+  printTime(date) {
+    let timing = formatDistanceToNow(date, {includeSeconds: true});
+    return `Created ${timing} ago`;
+  }
 
   deleteItem = (id) => {
     this.setState(({todoData}) => {
